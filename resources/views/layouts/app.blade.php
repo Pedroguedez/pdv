bfdef16c07239a6b520486f41843a1926202a031
elisandro <elisandro@farben.com.br>
2024-06-18 16:58:45 -0300

    Ajuste sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,60 +6,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title') - PG</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
   <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>
-  <div class="d-flex">
-    <nav class="bg-light" style="width: 250px; height:100vh;">
-      <header class="text-center mt-4">
-        <div class="image-text">
-          <img src="logo.png" alt="" class="rounded">
-          <div class="text logo-text ml-3">
-            <span class="name">Olá {{ auth()->user()->nome }}</span>
-          </div>
-        </div>
-      </header>
-      <div class="menu-bar p-3 ">
-        <div class="menu">
-          <ul class="menu-links list-unstyled">
-            <li class="nav-link">
-              <a href="#">
-                <i class="bx bx-home-alt icon"></i>
-                <span class="text nav-text ml-3">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="#">
-                <i class="bx bx-box icon"></i>
-                <span class="text nav-text ml-3">Produtos</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="#">
-                <i class="bx bx-archive icon"></i>
-                <span class="text nav-text ml-3">Estoque</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="{{ route('usuarios-index') }}">
-                <i class="bx bx-user icon"></i>
-                <span class="text nav-text ml-3">Usuários</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <footer class="mt-20">
-          <a href="{{ route('login.logout') }}">
-            <i class="bx bx-log-out icon"></i>
-            <span class="text nav-text ml-3">Logout</span>
-          </a>
-        </footer>
+  <div class="container-fluid">
+    <div class="row flex-nowrap">
+      @include('layouts.menuLeft')
+      <div class="col py-3">
+        @yield('content')
       </div>
-    </nav>
-
-    @yield('content')
+    </div>
 
     <script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
---
+++ resources/views/layouts/menuLeft.blade.php
@@ -0,0 +1,54 @@
+<div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+  <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <span class="fs-5 d-none d-sm-inline">Menu</span>
+    </a>
+    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+      <li class="nav-item">
+        <a href="#" class="nav-link align-middle px-0">
+          <i class="fas fa-tachometer-alt"></i> <span class="ms-1 d-none d-sm-inline">Inicio</span>
+        </a>
+      </li>
+      <li>
+        <a href="#submenu1" class="nav-link px-0 align-middle">
+          <i class="fas fa-cart-arrow-down"></i>
+          <p class="ms-1 d-none d-sm-inline">Vendas <small style="float:right;opacity:0.50">Diferencial</small></p>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link px-0 align-middle">
+          <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Pedidos</span></a>
+      </li>
+      <li>
+        <a href="#submenu2" class="nav-link px-0 align-middle">
+          <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Produtos</span> </a>
+      </li>
+      <li>
+        <a href="#submenu3" class="nav-link px-0 align-middle">
+          <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Relatorios</span> </a>
+      </li>
+      @auth
+      @if (auth()->user()->cargo == 'admin')
+      <li>
+        <a href="{{ route('usuarios-index') }}" class="nav-link px-0 align-middle">
+          <span class="ms-1 d-none d-sm-inline">Admin</span>
+        </a>
+      </li>
+      @endif
+      @endauth
+    </ul>
+    <hr>
+    <div class="dropdown pb-4">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <span class="d-none d-sm-inline mx-1">{{ auth()->user()->nome }}</span>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+        <li><a class="dropdown-item" href="{{ route('usuarios-edit' , ['id'=>auth()->user()->id]) }}">Profile</a></li>
+        <li>
+          <hr class="dropdown-divider">
+        </li>
+        <li><a class="dropdown-item" href="{{ route('login.logout') }}">Sign out</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
