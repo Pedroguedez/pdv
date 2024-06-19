d00e2f8fa9f538fc7149107a552f59348c2a9298
elisandro <elisandro@farben.com.br>
2024-06-19 10:37:03 -0300

    melhorias tela de usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -8,18 +8,17 @@
     <div class="col-sm-10">
       <h1>Listagem dos usuarios</h1>
     </div>
-    <div class="col-sm-2">
-      <a href="{{route('usuarios-create')}}" class="btn btn-success">Novo Usuario</a>
-    </div>
   </div>
-  <table class="table  table-hover">
+  <table class="table table-hover table-striped">
     <thead>
       <tr>
-        <th scope="col">id</th>
+        <th scope="col">#</th>
         <th scope="col">Nome</th>
         <th scope="col">Cargo</th>
-        <th scope="col">senha</th>
-        <th scope="col">...</th>
+        <th scope="col">Senha</th>
+        <th style="text-align:center"> <a href="{{ route('usuarios-create') }}" class="btn btn-sm btn-success" title="Novo Usuário">
+            <i class="fas fa-plus"></i> Novo Usuário
+          </a></th>
       </tr>
     </thead>
     <tbody>
@@ -29,21 +28,23 @@
         <td>{{$usuario->nome}}</td>
         <td>{{$usuario->cargo}}</td>
         <td>{{$usuario->password}}</td>
-        <td class="d-flex">
-          <a href="{{route('usuarios-edit', ['id'=>$usuario->id])}}" class="btn btn-primary me-2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-              <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-            </svg>
-          </a>
-          <form action="{{route('usuarios-destroy', ['id'=>$usuario->id])}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger ">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-              </svg>
-            </button>
-          </form>
+        <td style="text-align:center">
+          <div class="btn-group droup">
+            <button id="btnGroupDrop1" type="button" class="btn btn-sm btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-cogs"></i></button>
+            <ul class="dropdown-menu" aria-labelledby="btnGoupDrop1">
+              <li><a href="{{route('usuarios-edit', ['id'=>$usuario->id])}}" class="btn dropdown-item"><i class="fas fa-edit"> Editar</i></a></li>
+              <li>
+                <form action="{{ route('usuarios-destroy', ['id' => $usuario->id]) }}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button type="submit" class="dropdown-item">
+                    <i class="fas fa-trash-alt" style="color: #cc6666"></i> Excluir
+                  </button>
+                </form>
+              </li>
+
+            </ul>
+          </div>
         </td>
       </tr>
       @endforeach
